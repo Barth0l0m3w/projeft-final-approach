fc5e13c5d48705ccaebaa8a906d71d170a695e61
Nathalie <xnhogenbergx@gmail.com>
2022-04-26 01:28:51 -0700

    ultra GXP

---
+++ GXPEngine/MyGame.cs
@@ -0,0 +1,18 @@
+using System;									// System contains a lot of default C# libraries 
+using GXPEngine;                                // GXPEngine contains the engine
+using System.Drawing;							// System.Drawing contains drawing tools such as Color definitions
+using System.Collections.Generic;
+
+public class MyGame : Game
+{
+
+    public MyGame() : base(400, 400, false, false) 
+    {
+        
+    }
+
+    static void Main()
+    { 
+    
+    }
+}
