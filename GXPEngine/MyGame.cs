5ba81b16770db13c401fa64958fdcf9e01e6aaa3
Kamila Matuszak <kamila.matuszak@list.pl>
2022-04-28 16:33:10 +0200

    Merge fixed

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -11,20 +11,40 @@ public class MyGame : Game
 	string levelName = "map_prototype_big.tmx";
 	Levels level;
 
-    public MyGame() : base(1920, 1080, false, true, 960, 540)
+	public MyGame() : base(1920, 1080, false, true, 960, 540)
+	{
+        //// Draw some things on a canvas:
+        //EasyDraw canvas = new EasyDraw(800, 600);
+        //AddChild(canvas);
+
+        //spellRange = new SpellRange(3, 3, width / 2, height / 2);
+        //AddChild(spellRange);
+
+        //mushroom = new Mushroom();
+        //AddChild(mushroom);
+
+    }
+
+	void Update()
     {
-        // Draw some things on a canvas:
-        EasyDraw canvas = new EasyDraw(800, 600);
-        AddChild(canvas);
+		if (Input.GetKey(Key.SPACE))
+		{
+			targetFps = 5;
+		}
+		else
+		{
+			targetFps = 60;
+		}
 
-        spellRange = new SpellRange(3, 3, width / 2, height / 2);
-        AddChild(spellRange);
+		if (Input.GetKeyDown(Key.R))
+		{
+			Console.WriteLine("Reloading the level " + levelName);
+			LoadLevel(levelName);
+		}
+	}
 
-        mushroom = new Mushroom();
-        AddChild(mushroom);
 	static void Main()							// Main() is the first method that's called when the program is run
 	{
-		DoTests();
 		new MyGame().Start();					// Create a "MyGame" and start it
 	}
 
