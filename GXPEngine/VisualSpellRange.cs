263ae516dcac00e164aefabe9efedcefb4902b07
Nathalie <xnhogenbergx@gmail.com>
2022-04-29 14:14:47 -0700

    spellrange fixed and working
    having the spellrange be separate classes that you can adjust separately, VisualSpellRange is the thing you see and CollisionSpellRange gives the boolean properties

---
+++ GXPEngine/VisualSpellRange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+using TiledMapParser;
+
+
+public class VisualSpellRange : Sprite
+{
+
+
+    public VisualSpellRange(TiledObject obj = null) : base("checkers.png", false)
+    {
+        SetOrigin(width/2, height/2);
+    }
+}
+
