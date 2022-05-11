bbce3f98a37799013946b740c00e04e5cd68e92b
Kamila Matuszak <kamila.matuszak@list.pl>
2022-05-11 13:11:52 +0200

    NextLevel button appears in the middle

---
+++ GXPEngine/Star.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+using TiledMapParser;
+
+
+internal class Star : Sprite
+{
+    public Star(TiledObject obj = null) : base("star.png")
+    {
+
+    }
+}
+
