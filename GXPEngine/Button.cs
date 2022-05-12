1073fddee0c7c073b5c3a49c8179ed5801ee0e0d
Nathalie <xnhogenbergx@gmail.com>
2022-05-12 12:16:33 -0700

    music switching and button position fix

--- GXPEngine/Button.cs
+++ GXPEngine/Button.cs
@@ -9,24 +9,16 @@ using TiledMapParser;
 
 public class Button : Sprite
 {
-    private Vec2 Position
-    {
-        get
-        { return _position; }
-    }
-
     private Vec2 _position;
-    private Vec2 distance;
     private Vec2 mouseP;
+
     private bool clicked = false;
     private string levelName = null;
     private int function = 0;
-    //private int levelNumber = 1;
     private string image = null;
 
     public Button(TiledObject obj = null) : base("square.png")
     {
-        //SetOrigin(width / 2, height / 2);
         levelName = obj.GetStringProperty("levelName", null);
         function = obj.GetIntProperty("function", 0);
         image = obj.GetStringProperty("image", null);
@@ -41,31 +33,24 @@ public class Button : Sprite
 
     private void MouseTouching()
     {
-        if (distance.Length() <= width/2)
+        if (HitTestPoint(Input.mouseX, Input.mouseY))
         {
-
             if (Input.GetMouseButtonUp(0))
             {
                 clicked = true;
             }
         }
     }
+
     private void CurrentLoad()
     {
-
         SceneManager.Instance.LoadLevel(((MyGame)game).CurrentLevel);
-
     }
 
     private void GoLevel()
     {
         SceneManager.Instance.LoadLevel(levelName);
         ((MyGame)game).CurrentLevel = levelName;
-    }
-
-    private void LastLevel()
-    {
-        SceneManager.Instance.LoadLevel(levelName);
     }
 
     private void UpdateMousePosition()
@@ -76,7 +61,6 @@ public class Button : Sprite
 
     void Step()
     {
-        distance = mouseP - Position;
         MouseTouching();
         UpdateMousePosition();
         ChooseFunction();
@@ -87,8 +71,8 @@ public class Button : Sprite
     {
         if (((MyGame)game).mobHit && function == 0)
         {
-            x = 1588+64;
-            y = 916+64;
+            x = 1588 + 64;
+            y = 916 + 64;
         }
         if (((MyGame)game).startTorch && function == 2)
         {
@@ -107,13 +91,13 @@ public class Button : Sprite
                     GoLevel();
                 };
                 break;
-            case 1:
+            case 1: //reload
                 if (clicked)
                 {
                     CurrentLoad();
                 }
                 break;
-            case 2:
+            case 2: //Start torch
                 if (clicked)
                 {
                     ((MyGame)game).startTorch = true;
